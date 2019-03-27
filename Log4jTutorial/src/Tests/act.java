9eb7682806037d66864136f69909905753e7a1ab
Arun Mannepula <makprotractor@gmail.com>
2019-03-28 02:33:35 +0530

    updated framework with log4j and excel code

--- Log4jTutorial/src/Tests/act.java
+++ Log4jTutorial/src/Tests/act.java
@@ -2,7 +2,7 @@ package Tests;
 
 import org.apache.logging.log4j.*;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,15 +24,11 @@ public class act {
 		log.info("Landed on amazon home page");
 		Actions a = new Actions(driver);
 
-		/*
-		 * a.moveToElement(driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']")
-		 * ))
-		 * .click().keyDown(Keys.SHIFT).sendKeys("capitalletters").doubleClick().build()
-		 * .perform(); a.moveToElement(driver.findElement(By.cssSelector(
-		 * "a[id='nav-link-accountList']"))).build().perform();
-		 * a.moveToElement(driver.findElement(By.cssSelector(
-		 * "a[id='nav-link-accountList']"))).contextClick().build().perform();
-		 */
+		a.moveToElement(driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']"))).click().keyDown(Keys.SHIFT)
+				.sendKeys("capitalletters").doubleClick().build().perform();
+		a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
+		a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).contextClick().build()
+				.perform();
 
 		driver.get("http://jqueryui.com/demos/droppable/");
 		log.debug("Getting the frames count");
